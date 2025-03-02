599cec448193116ee667bbed4643f6a70efde242
Adil Dhada <aadildhada518@gmail.com>
2025-03-02 12:13:58 +0530

    feat: Added building block with CQRS and set depedency to catalog project

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+public interface ICommandHandler<in ICommand> : ICommandHandler<ICommand, Unit>
+    where ICommand : ICommand<Unit>;
+
+public interface ICommandHandler<in ICommand, TResponse> : IRequestHandler<ICommand, TResponse>
+    where ICommand : ICommand<TResponse>
+    where TResponse : notnull;
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,17 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Categories, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        //Todo: Add Business logic to create product
+        throw new NotImplementedException();
+    }
+}
